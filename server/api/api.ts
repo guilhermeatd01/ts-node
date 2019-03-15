4a51b7640a6059e9b006d927abb7ed1edffe284b
Guilherme Fonseca <guilherme.atd@hotmail.com>
2019-03-15 07:00:05 -0300

    Initial commit

---
+++ server/api/api.ts
@@ -0,0 +1,20 @@
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { Application } from 'express';
+import * as morgan from 'morgan';
+
+class Api {
+    public express: Application;
+
+    constructor() {
+        this.express = express();
+    }
+
+    middleware(): void {
+        this.express.use(morgan('dev'));
+        this.express.use(bodyParser.urlencoded({extended: true}));
+        this.express.use(bodyParser.json());
+    }
+}
+
+export default new Api().express;
